2260dc9f7de2061a934aef2b571e4f0526a73218
sheminith <10554sheminith@gmail.com>
2025-06-14 23:28:19 +0530

    Check color code function completed

--- color_guesser.py
+++ color_guesser.py
@@ -32,3 +32,27 @@ def guess_code():
 
     return guess
 
+# Check color code
+def check_code(guess, real_code):
+    color_counts = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    #['G', 'Y', 'B', 'O']
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_counts[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] > 0:
+            incorrect_pos += 1
+            color_counts[guess_color] -= 1
+
+    return correct_pos, incorrect_pos
